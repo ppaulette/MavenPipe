53372dd4355020ce812e840c305a175007213206
student <paulettephay@gmail.com>
2021-07-17 16:10:08 +0000

    Maven Pipeline test

---
+++ src/main/java/com/simplical/mavtest/MavtestApplication.java
@@ -0,0 +1,13 @@
+package com.simplical.mavtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavtestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simplical/mavtest/MavtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplical.mavtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
